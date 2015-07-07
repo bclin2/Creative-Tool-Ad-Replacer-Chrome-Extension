9038d6ddf2ebe79ffe3feff99540535fa67443b8
bclin2 <brian.c.lin2@gmail.com>
2015-07-07 16:49:32 -0700

    iframe parents can now be focused, keydown events on those iframe parents are acknowledged. Ideally, user can press ctrl on focused iframe parent and delete the iframe

--- content.js
+++ content.js
@@ -7,60 +7,43 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       alert("Ad Replacer is active.");
-      var originalBackgroundColor, adHeight, adWidth, blurListener;
+      var originalBackgroundColor, adHeight, adWidth;
       console.log("Content.js is running!");
-      // $("*").not("body, html").hover(function(e) {
-      //   if ($(this).css("background")) {
-      //     originalBackgroundColor = $(this).css("background");
-      //   } else {
-      //     originalBackgroundColor = "none";
-      //   }
-      //   $(this).css("border", "1px solid #000");
-      //   // $(this).css("background", "rgba(0,0,0,0.75)"; //needs to target overlay
-      //   e.stopPropagation();
-      //   adHeight = $(this).height();
-      //   adWidth = $(this).width();
-      //   // $(this).html(overlay);
-      //   // $('.overlay').css('background', 'rgba(0,0,0.75)');
-      //   console.log('width: ' + adWidth + ', height: ' + adHeight);
-      // }, function(e) {
-      //   $(this).css("border", "0px"); 
-      //   // $(this).css("background", originalBackgroundColor); //needs to target overlay
-      //   e.stopPropagation();
-      // });
-      
-      var iFrameCollection = document.getElementsByTagName('IFRAME');
-      var iFrameCounter = 0;
-      console.log(iFrameCollection);
-      while (iFrameCollection.length > 0) {
-        //each iteration removes the first element in iFrameCollection, so the next iFrame is the new first element
-        var adWidth = $(iFrameCollection[0]).width();         
-        var adHeight = $(iFrameCollection[0]).height();
-        console.log("width: " + adWidth + ", height: " + adHeight);
-        var $iFrameParent = $($(iFrameCollection[0]).parent());
-        $iFrameParent.html('<div class="placeholder" id="iframe-' + iFrameCounter + '" style="height: ' + adHeight + 'px; background-color: rgba(0,0,0,0.85)"></div>');
-        iFrameCounter++;
-      }
+
+      $('iframe').parent().hover(function(e) {
+        if ($(this).css("background")) {
+          originalBackgroundColor = $(this).css("background");
+        } else {
+          originalBackgroundColor = "none";
+        }
+        $(this).css("border", "3px solid yellow");
+        e.stopPropagation();
+        adHeight = $(this).height();
+        adWidth = $(this).width();
+        console.log('width: ' + adWidth + ', height: ' + adHeight);
+        console.log($(this));
+        // debugger;
+        $(this).attr('tabindex', '0');
+        $(this).focus();
+      }, function(e) {
+        $(this).css("border", "0px"); 
+        e.stopPropagation();
+        $(this).blur();
+      }).on('keydown', function(event) {
+        var ctrl = 17;
+        console.log('Keydown recognized');
+        if (event.keyCode === ctrl) {
+          console.log('keydown for ctrl recognized')
+          
+        }
+      });
+
+
+
 
       //PROBLEMS: for the first iFrame, adHeight isn't being applied correctly
                 //By removing the iFrame, the collection is also being removed
 
-      // focus();
-      // blurListener = addEventListener('blur', function() {
-      //   if (document.activeElement === document.getElementsByTagName('iframe')) {
-      //     console.log("click event SUCCESSFULLY CAPTURED")
-      //   }
-      // });
-
-      // $('*').not("body, html").on('click', function(e){
-      //   e.preventDefault();
-      //   e.stopPropagation();
-      //   e.stopImmediatePropagation();
-      //   adHeight = $(this).height();
-      //   adWidth = $(this).width();
-      //   console.log("click event registered");
-
-      // });
     }
   }
 );
