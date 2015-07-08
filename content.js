7b3738c4558e23c07cb6040d474d5bd905831c2c
bclin2 <brian.c.lin2@gmail.com>
2015-07-07 17:43:52 -0700

    iframe keyboard press working, but i need to click

--- content.js
+++ content.js
@@ -7,34 +7,31 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       alert("Ad Replacer is active.");
-      var originalBackgroundColor, adHeight, adWidth;
+      var adHeight, adWidth, placeholder, placeholderDimensions;
       console.log("Content.js is running!");
 
-      $('iframe').parent().hover(function(e) {
-        if ($(this).css("background")) {
-          originalBackgroundColor = $(this).css("background");
-        } else {
-          originalBackgroundColor = "none";
-        }
-        $(this).css("border", "3px solid yellow");
+      $('iframe').parent().not('body, html').hover(function(e) {
+        $(this).children('iframe').css("border", "3px solid yellow");
         e.stopPropagation();
-        adHeight = $(this).height();
-        adWidth = $(this).width();
+        adHeight = $(this).children('iframe').height();
+        adWidth = $(this).children('iframe').width();
         console.log('width: ' + adWidth + ', height: ' + adHeight);
         console.log($(this));
-        // debugger;
         $(this).attr('tabindex', '0');
         $(this).focus();
       }, function(e) {
-        $(this).css("border", "0px"); 
+        $(this).children('iframe').css("border", "0px"); 
         e.stopPropagation();
         $(this).blur();
       }).on('keydown', function(event) {
         var ctrl = 17;
         console.log('Keydown recognized');
+        placeholderDimensions = '<div class="placeholderDimensions" style="color: white; font-size: 1vw; text-align: center; padding-top: 20%">' + adWidth + 'X' + adHeight + '</div>';
+        placeholder = '<div class=placeholder style="background-color: rgb(0,0,0); height: ' + adHeight + 'px; width: ' + adWidth + 'px">' + placeholderDimensions + '</div>';
+
         if (event.keyCode === ctrl) {
-          console.log('keydown for ctrl recognized')
-          
+          console.log('keydown for ctrl recognized');
+          $(this).html(placeholder);
         }
       });
 
