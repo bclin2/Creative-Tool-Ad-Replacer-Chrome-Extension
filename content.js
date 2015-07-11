558dd1928ea6eb65cbbeb26ff6585428a813f99d
bclin2 <brian.c.lin2@gmail.com>
2015-07-10 17:39:01 -0700

    deleted old obsolete not working commented code; deleted some comments

--- content.js
+++ content.js
@@ -90,15 +90,9 @@ $('body').on({
       }
       $topOfStack = $(elementsStack[0]);
 
-      //stop undefined from being pushed to either stack
-
       console.log("after: ", elementsStack);
       console.log("keyDownStack: ", keyDownStack);
       console.log("TopOfStack: ", $topOfStack);
-      //remove overlay
-
-      //REAL problem is why $topOfStack sometimes reads as 0x0
-
       removeOverlay();
       renderOverlay();
 
@@ -139,166 +133,14 @@ chrome.runtime.onMessage.addListener(
         }
 
         //set overlay under body and find the position of the $topOfStack
-        // console.log($(topOfStack));
-
         renderOverlay();
 
         console.log("stack:", elementsStack);
         console.log("element: ", $topOfStack);
         console.log("x:", mouseCoordinateX, "y:", mouseCoordinateY);
 
-        //remove overlay on everything else
       })
-
-      // problems: 
-        //divHeight
-
-
-      // $('body').on('mouseenter.creativeTool', '*:not(.inspectOverlay):not(.overlayDimensions)', function(event) {
-      //   selectedElement.push($(event.target));
-      //   updateSelectedElement();
-      // })
-      // .on('mouseleave.creativeTool', '*:not(.inspectOverlay):not(.overlayDimensions)', function(event) {
-      //   selectedElement.pop();
-      //   updateSelectedElement();
-      // });
-
-      /*var inspectOverlayTemplates = function(divWidth, divHeight) {
-        this._inspectOverlay = '<div class="inspectOverlay" style="position: absolute; opacity: 0.75; background-color: #93CFF4; height: ' + divHeight + 'px; width: ' + divWidth + 'px"></div>';
-        this._overlayDimensions = '<div class="overlayDimensions" style="position: relative, z-index: 1200; background-color: yellow; color: black; font-size: 1vw; text-align: center; opacity: 1.0">' + divWidth + 'X' + divHeight + '</div>';
-      };
-
-      var divObject = function(jQueriedDiv) {
-        this.$div = jQueriedDiv;
-        this.renderOverlay = function() {
-          var divWidth = this.$div.width();
-          var divHeight = this.$div.height();
-          inspectOverlay = new inspectOverlayTemplates(divWidth, divHeight);
-          this.$div.prepend(inspectOverlay._inspectOverlay);
-          $('.inspectOverlay').html(inspectOverlay._overlayDimensions);
-        };
-        this.removeOverlay = function() {
-          this.$div.children().remove('.inspectOverlay');
-        }
-      };
-
-      var divStack = function() {
-        this.stack = [];
-        this.$currentFocus;
-        this.updateFocus = function() {
-          this.$currentFocus = this.stack[this.stack.length - 1];
-          if (this.$currentFocus) {
-            this.$currentFocus.renderOverlay();
-          }
-        };
-        this.push = function(div) {
-          this.stack.push(div);
-          if (this.$currentFocus) {
-            this.$currentFocus.removeOverlay();
-          }
-          this.updateFocus();
-        };
-        this.pop = function() {
-          if (this.$currentFocus) {
-            this.$currentFocus.removeOverlay();
-          }
-          this.stack.pop();
-          this.updateFocus();
-        }; 
-      };
-
-      var globalStackTracker = new divStack();
-
-      $('*').not('body, html, .inspectOverlay, .overlayDimensions').on({
-        mouseenter: function(e) {
-          e.stopPropagation();
-          e.preventDefault();
-          currentDiv = new divObject($(this));
-          globalStackTracker.push(currentDiv);
-          console.log(globalStackTracker.stack[globalStackTracker.stack.length - 1]);
-        },
-        mouseleave: function(e) {
-          globalStackTracker.pop();
-        },
-        click: function(e) {
-          e.preventDefault();
-        }
-      });*/
-      
-
-
-
-      // $('*').not('body, html, .inspectOverlay, .overlayDimensions').hover(function(e) {
-      //   e.stopPropagation();
-      //   e.preventDefault();
-      //   if ($(this).is('iframe')) {
-      //     containsiFrame = true;
-      //     console.log("IFRAME DETECTED", containsiFrame)
-      //     $iFrameParentDiv = $(this).parent().not('body, html');
-      //     $iFrameParentDiv.attr('tabindex', '0');
-      //     $iFrameParentDiv.focus();
-      //     divHeight = $iFrameParentDiv.height();
-      //     divWidth = $iFrameParentDiv.width();
-      //   } else {
-      //     divHeight = $(this).height();
-      //     divWidth = $(this).width();  
-      //   }
-
-      //   $(this).css('border', '1px solid black');
-      //   inspectOverlay = '<div class=inspectOverlay style="position: absolute; opacity: 0.75; z-index: 1000; background-color: #93CFF4; height: ' + divHeight + 'px; width: ' + divWidth + 'px"></div>';
-      //   overlayDimensions = '<div class="overlayDimensions" style="position: relative, z-index: 1200; background-color: yellow; color: black; font-size: 1vw; text-align: center; opacity: 1.0">' + divWidth + 'X' + divHeight + '</div>';
-
-      //   $(this).prepend(inspectOverlay);
-      //   $('.inspectOverlay').html(overlayDimensions);
-
-      //   console.log('This:', $(this));
-      //   console.log('children: ', $(this).children());
-      // }, function(e) {
-      //   $(this).css('border', "0px");
-      //   if (containsiFrame) {
-      //     containsiFrame = false;
-      //     $iFrameParentDiv.blur();
-      //     console.log('LEAVING IFRAME', containsiFrame);
-      //   }
-      //   $($(this).children('.inspectOverlay')).remove();
-      //   $($(this).children('.overlayDimensions')).remove();
-
-      // });
-
-      //     .on('keydown', function(event) {
-      //     var ctrl = 17;
-      //     console.log('Keydown recognized');
-      //     overlayDimensions = '<div class="overlayDimensions" style="color: white; font-size: 1vw; text-align: center; padding-top: 20%">' + divWidth + 'X' + divHeight + '</div>';
-      //     placeholder = '<div class=placeholder style="background-color: rgb(0,0,0); height: ' + divHeight + 'px; width: ' + divWidth + 'px">' + overlayDimensions + '</div>';
-
-      //     if (event.keyCode === ctrl && containsiFrame) {
-      //       console.log('keydown for ctrl recognized');
-      //       $iFrameParentDiv.html(placeholder);
-      //     }
-
-      // });
-
-
-      //PROBLEM: 
-        //The problem is that it covers the entire parent div on mouseover. We can't see the smaller divs that become highlighted. 
-          //Increase z-index of all child divs?
-        //Problem is how hover works. Remember the hover border technique and how the border stays until I move completely out of the div
-          //I need to find a way to change focus
-        //Also, I can't overlay over iframes because of the same origin policy
-          //If the iframes get their ads from a different domain, then I can't interact with the divs
-        //images in a list don't get inspected; the overlay gets appended to the image, not the <li> itself
-          //causes graphical errors
-
 
     }
   }
 );
-
-
-
-
-
-
-
-//commit where tool works like inspect for chrome devtools
-//454b7e644abc2de3970def51954eb1e05c3de42a
