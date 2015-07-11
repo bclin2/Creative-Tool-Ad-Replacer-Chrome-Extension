ebc068679b5d4effaa91e4d0011de3595780b53a
bclin2 <brian.c.lin2@gmail.com>
2015-07-10 17:55:09 -0700

    reformatted some things; deleted comments and useless code

--- content.js
+++ content.js
@@ -2,10 +2,6 @@
 // alert("Hello from the chrome extension!");
 // HAVING default_popup ENABLED IN THE manifest.json causes all other js to not run
 // capturing click events on an iframe from a different domain is impossible
-//The position
-
-//for overlay, in case I want to try the tooltip. Needs bootstrap.
-//data-toggle="tooltip" title="TESTDIV"
 
 var $overlay = $('<div class="inspectOverlay" style="position: absolute; background-color: rgba(255, 255, 0, 0.4); z-index: 99999999;"></div>');
 var $dimensions = $('<div class="overlayDimensions" style="position: relative, z-index: 100000000; background-color: yellow; color: black; font-size: 1vw; text-align: center; opacity: 1.0"></div>');
@@ -20,24 +16,19 @@ var overlayDimensions;
 
 function disableArrowKeys() {
   window.addEventListener("keydown", function(e) {
-      // arrow keys
-      if ([arrowUp].indexOf(e.keyCode) > -1) {
-          e.preventDefault();
-      }
+    if ([arrowUp].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+    }
   }, false);
 };
 
 function renderOverlay() {
   $('body').append($overlay);
-  // debugger;
   offset = $topOfStack.offset();
-  //maybe use a combination of position and offset to get the exact position
   divHeight = $topOfStack.innerHeight();
   divWidth = $topOfStack.innerWidth();
-  //REAL problem is why $topOfStack sometimes reads as 0x0
 
   if (divHeight === 0 || divWidth === 0) {
-    debugger;
     return;
   }
   $overlay.css({
@@ -64,7 +55,7 @@ $('body').on({
     //Set focus on overlay so keydowns can be captured
     $(this).attr('tabindex', '0');
     $(this).focus();
-    //disable up and down keydowns
+    //disable up keydowns
     disableArrowKeys();
     console.log('clicked');
   }, 
@@ -73,10 +64,6 @@ $('body').on({
     var pendingTopOfStack;
     //capture keydowns
     if (event.keyCode === arrowUp) {
-      //go up DOM tree
-        //pop off $topOfStack and insert into keyDownStack
-      console.log("ARROW UP!!!!");
-      console.log("before: ", elementsStack);
 
       pendingTopOfStack = elementsStack.shift();
 
@@ -89,9 +76,6 @@ $('body').on({
       }
       $topOfStack = $(elementsStack[0]);
 
-      console.log("after: ", elementsStack);
-      console.log("keyDownStack: ", keyDownStack);
-      console.log("TopOfStack: ", $topOfStack);
       removeOverlay();
       renderOverlay();
 
@@ -111,12 +95,11 @@ chrome.runtime.onMessage.addListener(
 
       $('body').css('cursor', 'crosshair');
 
-      //debugging purposes, removes all hrefs from anchor
+      //debugging purposes, removes all hrefs from anchors
       $('a').removeAttr('href');
 
       $('div').not('body, html').mousemove(function(event) {
         event.stopPropagation();
-        // $('[data-toggle="tooltip"]').tooltip();
 
         //get coordinates
         //subtracted offset to fix scrolling issue
@@ -131,7 +114,6 @@ chrome.runtime.onMessage.addListener(
           $topOfStack = $(elementsStack[0]);
         }
 
-        //set overlay under body and find the position of the $topOfStack
         renderOverlay();
 
         console.log("stack:", elementsStack);
