28019a682879b1086c15c8fe10a909f93a8ebd69
bclin2 <brian.c.lin2@gmail.com>
2015-07-21 13:51:50 -0700

    added connection to background, saved current filter state

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+var port = chrome.runtime.connect({name: "popupToBackground"});
 
 function imageReplacer() {
   var imageReplacerButton = document.getElementById('imageReplacer');
@@ -10,16 +11,44 @@ function imageReplacer() {
 
 //Need to change to a checkbox or on/off switch
 //talk to background.js to remove/add event filter listener
-function videoReplacer() {
+
   // var videoReplacerButton = document.getElementById('videoReplacer');
   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   //   var activeTab = tabs[0];
   //   chrome.tabs.sendMessage(activeTab.id, {"message": "video_replacer_activated"});
   // });
-  window.close();
+  // document.getElementById('videoReplacer').checked;
+
+function videoToggle() { 
+  var checked = document.getElementById('videoToggle').checked;
+  console.log("STATUS: ", checked);
+  chrome.storage.local.set({"videoToggleStatus": checked});
+  port.postMessage({videoToggle: checked});
 };
+
+function checkAndAssignVideoToggleStatus() {
+  var toggle;
+  chrome.storage.local.get("videoToggleStatus", function(obj) {
+    console.log("...can anyone hear me?");
+    toggle = obj;
+    console.log(toggle);
+    if (toggle) {
+      console.log("toggle exists!")
+      if (toggle.videoToggleStatus) {
+        console.log("toggle is true!");
+        document.getElementById('videoToggle').checked = true;
+      } else {
+      document.getElementById('videoToggle').checked = false;
+      }
+    }
+  });
+};
+
+checkAndAssignVideoToggleStatus();
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('imageReplacer').addEventListener("click", imageReplacer);
-  document.getElementById('videoReplacer').addEventListener("click", videoReplacer);
+  document.getElementById('videoToggle').addEventListener('click', videoToggle);
+
+  // document.getElementById('videoReplacer').addEventListener("click", videoReplacer);
 });
