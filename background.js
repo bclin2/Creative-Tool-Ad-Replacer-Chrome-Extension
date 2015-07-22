f8ca3c44761b96429b4b15dc1bb95c71ff6daa42
bclin2 <brian.c.lin2@gmail.com>
2015-07-22 15:43:46 -0700

    integrated messaging with webRequest API, the VAST redirect. Put VAST redirect callback into a function so I could remove the listener later. Lots of bugs, may need to restructure the way ports talk to each other. Will continue in another branch. It technically works.

--- background.js
+++ background.js
@@ -4,86 +4,88 @@ chrome.storage.local.clear(function() {
   console.log("CLEARED CHROME STORAGE!!!!!");
 });
 
-// chrome.runtime.onConnect.addListener(function(port) {
-//   console.log("Connected...", port);
-//   //connect to content.js
-//   var backgroundPort;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     var activeTab = tabs[0];
-//     backgroundPort = chrome.tabs.connect(activeTab.id, {name: "backgroundToContent"});
-//   });
-//   port.onMessage.addListener(function(message) {
-//     console.log("message received: ", message);
-//     if (message.videoToggle) {
-//       console.log("Activating onHeadersReceived Listeners...");
-//       //addListeners for filters
-//       backgroundPort.postMessage({videoToggleListenersActive: true});
-//     } else {
-//       console.log("Deacivating onHeadersReceived Listeners");
-//       //removeEventListeners for Filters
-//     }
+// var testRedirectURL = "http://demo.tremorvideo.com/proddev/vast/vast_inline_linear.xml";
+var redirectUrl;
 
-//     if (message.redirectURL) {
-//       console.log("content.js: ", message.redirectURL);
-//       //receive redirectURL and tell listeners to redirect to it
-//     }
-//   });
-// });
+var vastRedirectAddListener = function(details) {
+  if (details.type === "script") {
+    return;
+  }
+  var blockingResponse = {};
+  var responseHeaders = details.responseHeaders;
+  var statusFlag = false;
+  var locationFlag = false;
+
+  console.log(details);
+
+  for (var i = 0; i < responseHeaders.length; i++) {
+    if (responseHeaders[i].name === "Status" || responseHeaders[i].name === "status") {
+      responseHeaders[i].value = "302";
+      statusFlag = true;
+    } else if (responseHeaders[i].name === "Location" || responseHeaders[i].name === "location") {
+      responseHeaders[i].value = redirectUrl;
+      locationFlag = true;
+    } 
+  }
+  if (!locationFlag) {
+    responseHeaders.push({"name": "Location", "value": redirectUrl});
+  }
+  if (!statusFlag) {
+    responseHeaders.push({"name": "Status", "value": "302"});
+  }
+  //not adding the responseHeaders seem to make no difference
+  blockingResponse.responseHeaders = responseHeaders;
+  blockingResponse.redirectUrl = redirectUrl;
+  return blockingResponse;
+};
+
+
+chrome.runtime.onConnect.addListener(function(port) {
+  console.log("Connected...", port);
+  //connect to content.js
+  var backgroundPort;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    backgroundPort = chrome.tabs.connect(activeTab.id, {name: "backgroundToContent"});
+  });
+  port.onMessage.addListener(function(message) {
+    console.log("message received: ", message);
+    if (message.videoToggle) {
+      console.log("Activating onHeadersReceived Listeners...");
+      //send request for redirectUrl from content.js
+      backgroundPort.postMessage({videoToggleListenersActive: true});
+    } else {
+      console.log("Deacivating onHeadersReceived Listeners");
+      //removeEventListeners for Filters
+      chrome.webRequest.onHeadersReceived.removeListener(vastRedirectAddListener);
+      // backgroundPort.postMessage({videoToggleListenersActive: false});
+      redirectURL = null;
+    }
+
+    if (message.redirectUrl) {
+      console.log("content.js: ", message.redirectUrl);
+      //receive redirectURL
+      redirectUrl = message.redirectUrl;
+      // console.log(redirectUrl);
+      //add event listener 
+      chrome.webRequest.onHeadersReceived.addListener(
+        vastRedirectAddListener,
+        {urls: [ '*://*.telemetryverification.net/*',
+         '*://*.vindicosuite.com/*',
+         '*://*.tidaltv.com/*',
+         '*://*.mookie1.com/*',
+         '*://*.doubleclick.net/*',
+         '*://rtr.innovid.com/r1*',
+         '*://*.freewheel.tv/*',
+         '*://optimized-by.rubiconproject.com/a/api/vast*',
+         '*://uswvideo.adsrvr.org/data/vast/*'
+        ]},  //event filtering 
+        ["blocking", "responseHeaders"]
+      );  
+    }
+  });
+});
 
 //CHECK if event filtering is set to on/off
   //if ON, set event filters to ON, tell content.js to reload the page, and prompt users for a redirect URL in content.js
   //DEFAULT is OFF
-
-// function vastRedirect() {
-//   //Let user know the video ads have been replaced with alert
-//   //replace video ad VAST request
-//   //delete redirectURL from storage
-
-// }
-
-var testRedirectURL = "http://demo.tremorvideo.com/proddev/vast/vast_inline_linear.xml";
-
-var testRedirectURL2 = "http://demo.tremorvideo.com/proddev/vast/vast_wrapper_linear_1.xml"
-chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    var blockingResponse = {};
-    var responseHeaders = details.responseHeaders;
-    var statusFlag = false;
-    var locationFlag = false;
-
-    if (details.type === "script") {
-      return;
-    }
-
-    for (var i = 0; i < responseHeaders.length; i++) {
-      if (responseHeaders[i].name === "Status" || responseHeaders[i].name === "status") {
-        responseHeaders[i].value = "302";
-        statusFlag = true;
-      } else if (responseHeaders[i].name === "Location" || responseHeaders[i].name === "location") {
-        responseHeaders[i].value = testRedirectURL;
-        locationFlag = true;
-      } 
-    }
-    if (!locationFlag) {
-      responseHeaders.push({"name": "Location", "value": testRedirectURL});
-    }
-    if (!statusFlag) {
-      responseHeaders.push({"name": "Status", "value": "302"});
-    }
-    console.log(details);
-    blockingResponse.responseHeaders = responseHeaders;
-    blockingResponse.redirectUrl = testRedirectURL;
-    return blockingResponse;
-  },
-  {urls: [ '*://*.telemetryverification.net/*',
-   '*://*.vindicosuite.com/*',
-   '*://*.tidaltv.com/*',
-   '*://*.mookie1.com/*',
-   '*://*.doubleclick.net/*',
-   '*://rtr.innovid.com/r1*',
-   '*://*.freewheel.tv/*',
-   '*://optimized-by.rubiconproject.com/a/api/vast*',
-   '*://uswvideo.adsrvr.org/data/vast/*'
-  ]},  //event filtering 
-  ["blocking", "responseHeaders"]
-);  
