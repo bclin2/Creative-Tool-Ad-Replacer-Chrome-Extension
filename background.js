115706290e9d056bc6a6f2b6d235dd32aacc6831
bclin2 <brian.c.lin2@gmail.com>
2015-07-22 12:18:40 -0700

    commented out port channels to test out VAST capture, it now works for some sites. Still have problems with different protocols; can't capture VAST requests if the site uses https and the replacement VAST url is http

--- background.js
+++ background.js
@@ -1,38 +1,34 @@
 // background.js
-
-
-//on reload check local storage for redirectURL
-//Have to check if redirectURL exists, if it does it means user is using video replacer and not image replacer
-// var redirectURL;
-// chrome.storage.local.get('redirectURL', function(result) {
-//   redirectURL = result;
-//   console.log(result);
-// })
-
-// var backgroundPort;
-
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   var activeTab = tabs[0];
-//   // console.log("Icon has been clicked!");
-//   // alert("Hello from background.js");
-//   // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-//   var backgroundPort = chrome.tabs.connect(activeTab.id, {name: "backgroundToContent"});
-// });
-
-chrome.runtime.onConnect.addListener(function(port) {
-  console.log("Connected...", console.log(port));
-  port.onMessage.addListener(function(message) {
-    console.log("message received: ", message);
-    if (message.videoToggle) {
-      console.log("Activating onHeadersReceived Listeners...");
-      //postMessage to content.js 
-      port.postMessage({videoToggleListenersActive: true});
-    } else if (message.test === "FROM CONTENT.JS") {
-      // console.log("Deacivating onHeadersReceived Listeners");
-      console.log("content.js: ", message.test);
-    }
-  });
+//chrome storage reset
+chrome.storage.local.clear(function() {
+  console.log("CLEARED CHROME STORAGE!!!!!");
 });
+
+// chrome.runtime.onConnect.addListener(function(port) {
+//   console.log("Connected...", port);
+//   //connect to content.js
+//   var backgroundPort;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     backgroundPort = chrome.tabs.connect(activeTab.id, {name: "backgroundToContent"});
+//   });
+//   port.onMessage.addListener(function(message) {
+//     console.log("message received: ", message);
+//     if (message.videoToggle) {
+//       console.log("Activating onHeadersReceived Listeners...");
+//       //addListeners for filters
+//       backgroundPort.postMessage({videoToggleListenersActive: true});
+//     } else {
+//       console.log("Deacivating onHeadersReceived Listeners");
+//       //removeEventListeners for Filters
+//     }
+
+//     if (message.redirectURL) {
+//       console.log("content.js: ", message.redirectURL);
+//       //receive redirectURL and tell listeners to redirect to it
+//     }
+//   });
+// });
 
 //CHECK if event filtering is set to on/off
   //if ON, set event filters to ON, tell content.js to reload the page, and prompt users for a redirect URL in content.js
@@ -45,41 +41,49 @@ chrome.runtime.onConnect.addListener(function(port) {
 
 // }
 
+var testRedirectURL = "http://demo.tremorvideo.com/proddev/vast/vast_inline_linear.xml";
 
+var testRedirectURL2 = "http://demo.tremorvideo.com/proddev/vast/vast_wrapper_linear_1.xml"
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    var blockingResponse = {};
+    var responseHeaders = details.responseHeaders;
+    var statusFlag = false;
+    var locationFlag = false;
 
+    if (details.type === "script") {
+      return;
+    }
 
-// chrome.webRequest.onHeadersReceived.addListener(
-//   function(data) {
-//     // if (data.url.includes("telemetry") || data.url.includes("innovid")) {
-//     //   console.log("received: ", data);
-//     // }
-
-//     $.each($(data.responseHeaders), function(index, header) {
-//       // console.log(header.value);
-//       if (header.name.includes("Content-Type") && header.value.includes('xml') && !data.url.includes('crossdomain')) {
-
-//         //I KNOW this is a vast response
-//         if (data.url.includes('vast')) {
-//           console.log(data.url);
-//         }
-
-//         //possibly a vast reponse
-//         console.log(data.url);
-//       }
-//     });
-//   },
-//   {urls: ["<all_urls>"]},
-//   ["responseHeaders"]
-// );  
-
-// Called when the user clicks on the browser action.
-// Doesn't console log anything, but alerts work
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    // console.log("Icon has been clicked!");
-    // alert("Hello from background.js");
-    // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
+    for (var i = 0; i < responseHeaders.length; i++) {
+      if (responseHeaders[i].name === "Status" || responseHeaders[i].name === "status") {
+        responseHeaders[i].value = "302";
+        statusFlag = true;
+      } else if (responseHeaders[i].name === "Location" || responseHeaders[i].name === "location") {
+        responseHeaders[i].value = testRedirectURL;
+        locationFlag = true;
+      } 
+    }
+    if (!locationFlag) {
+      responseHeaders.push({"name": "Location", "value": testRedirectURL});
+    }
+    if (!statusFlag) {
+      responseHeaders.push({"name": "Status", "value": "302"});
+    }
+    console.log(details);
+    blockingResponse.responseHeaders = responseHeaders;
+    blockingResponse.redirectUrl = testRedirectURL;
+    return blockingResponse;
+  },
+  {urls: [ '*://*.telemetryverification.net/*',
+   '*://*.vindicosuite.com/*',
+   '*://*.tidaltv.com/*',
+   '*://*.mookie1.com/*',
+   '*://*.doubleclick.net/*',
+   '*://rtr.innovid.com/r1*',
+   '*://*.freewheel.tv/*',
+   '*://optimized-by.rubiconproject.com/a/api/vast*',
+   '*://uswvideo.adsrvr.org/data/vast/*'
+  ]},  //event filtering 
+  ["blocking", "responseHeaders"]
+);  
